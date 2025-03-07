75cae1295c5ad2fd118e63fe97388832f4d627b4
SlapYourEnermy Developer <developer@slapyourenermy.com>
2025-03-07 20:33:32 +0300

    5

--- init_env.py
+++ init_env.py
@@ -1,6 +1,14 @@
 import sys
+import os
 
-sys.path.extend(["/stable-diffusion-webui-forge"])
+print("Current working directory:", os.getcwd())
+print("sys.path before:", sys.path)
+
+sys.path.extend(["/stable-diffusion-webui-forge-main"])
+
+print("sys.path after:", sys.path)
+print("Checking if directory exists:", os.path.exists("/stable-diffusion-webui-forge-main"))
+print("Listing directory:", os.listdir("/stable-diffusion-webui-forge-main") if os.path.exists("/stable-diffusion-webui-forge-main") else "Directory does not exist")
 
 from modules import launch_utils
 
