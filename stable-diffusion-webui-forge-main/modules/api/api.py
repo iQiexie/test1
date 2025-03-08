fd3d2bee48674d7eb1704067b6fe6b9ab0e9344b
iQiexie <email@email.com>
2025-03-08 15:46:56 +0300

    logs

--- stable-diffusion-webui-forge-main/modules/api/api.py
+++ stable-diffusion-webui-forge-main/modules/api/api.py
@@ -444,6 +444,7 @@ class Api:
 
     def text2imgapi(self, txt2imgreq: models.StableDiffusionTxt2ImgProcessingAPI):
         print(f"TEST TEST TEST\n\n\n\n\n\n\n{txt2imgreq.dict()=}\n\n\n\n\n\n\n")
+        raise RuntimeError(f"test test: {txt2imgreq.dict()=}")
         task_id = txt2imgreq.force_task_id or create_task_id("txt2img")
 
         script_runner = scripts.scripts_txt2img
