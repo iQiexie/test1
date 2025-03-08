0a37a75c06cf98baed123c21814596ed7bf3cf2a
iQiexie <email@email.com>
2025-03-08 14:22:21 +0300

    add logs

--- stable-diffusion-webui-forge-main/extensions-builtin/sd_forge_lora/networks.py
+++ stable-diffusion-webui-forge-main/extensions-builtin/sd_forge_lora/networks.py
@@ -13,6 +13,7 @@ from backend.patcher.lora import model_lora_keys_clip, model_lora_keys_unet, loa
 
 
 def load_lora_for_models(model, clip, lora, strength_model, strength_clip, filename='default', online_mode=False):
+    raise RuntimeError("TESTTEST TESTTEST TESTTEST TESTTESTTESTTEST TESTTESTTESTTEST TESTTESTTESTTEST TESTTEST")
     model_flag = type(model.model).__name__ if model is not None else 'default'
 
     unet_keys = model_lora_keys_unet(model.model) if model is not None else {}
