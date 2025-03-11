f5d83cd17533f7cacd43bb3921f1f011b4681a07
iQiexie <email@email.com>
2025-03-12 01:22:11 +0300

    Revert "fix imports"
    This reverts commit 13a63dec1e426f64ecb227faf50e426ad18bd873.

--- predict.py
+++ predict.py
@@ -1,21 +1,3 @@
-import sys
-import os
-
-sys.argv.append("--skip-torch-cuda-test")
-print("Current working directory:", os.getcwd())
-print("sys.path before:", sys.path)
-
-sys.path.extend(["/src"])
-
-print("sys.path after:", sys.path)
-print("Checking if directory exists:", os.path.exists("/src"))
-print("Listing directory:", os.listdir("/src") if os.path.exists("/src") else "Directory does not exist")
-
-from modules import launch_utils
-
-with launch_utils.startup_timer.subcategory("prepare environment"):
-    launch_utils.prepare_environment()
-
 from modules import initialize_util
 from modules import initialize
 from modules import timer
