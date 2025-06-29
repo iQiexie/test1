025a241539de88123bc9525c04331af0d2d35c63
user <email@email.com>
2025-06-29 18:12:38 +0300

    Revert "[prev commit works] fix live_preview"
    This reverts commit 9a7e9b78ad3812c18abcbc6ee1d0d658f8a987ef.

--- predict.py
+++ predict.py
@@ -505,13 +505,13 @@ class Predictor(BasePredictor):
                     response = self.api.progressapi(ProgressRequest(skip_current_image=False))
                     data = response.dict()
 
-                    if data.get("current_images"):
+                    if data.get("current_image") or data.get("current_images"):
                         requests.post(
                             url=postback_url or "https://back-dev.recrea.ai/api/v1/live_preview",
                             json=data,
                             verify=False,
                         )
-                    time.sleep(1)
+                    time.sleep(5)
                 except Exception as e:
                     print(f"[progress] got: {e=}")
 
